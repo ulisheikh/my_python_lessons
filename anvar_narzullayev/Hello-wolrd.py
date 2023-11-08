4a6dc977eab3036580722389be544c08dd12a320
ulisheikh <umahmudov034@gmail.com>
2023-11-08 15:48:10 +0300

    file updated

--- anvar_narzullayev/Hello-wolrd.py
+++ anvar_narzullayev/Hello-wolrd.py
@@ -17,3 +17,8 @@ O'larmidi payg'ambar""")
     #ikkinchi usuli "\n" yordamida qatorlarga bo'lish
 
 print("\nO'lim asli go'zaldir \nParda ortidan xabar \nGo'zal bo'lmasa edi \nO'larmidi payg'ambar")
+
+# cmd da kodlarni python orqali ochish 
+    # C:\Users\uli>python d:\GitHub\my_python_lessons\anvar_narzullayev\Hello-wolrd.py
+
+    
