7a76ab21264c43802dbb28f8db94c1465f7c84b2
ulisheikh <umahmudov034@gmail.com>
2023-11-08 15:27:59 +0300

    update

--- anvar_narzullayev/Hello-wolrd.py
+++ anvar_narzullayev/Hello-wolrd.py
@@ -5,3 +5,15 @@ Created on Wed Nov  8 14:45:02 2023
 @author: uli
 """
 print("Hello World")
+
+# Qatorlarga bo'lish usullari
+    # birinchi usul 3talik qo'shtirnoqdan foydalanish 
+    
+print("""\nO'lim asli go'zaldir 
+Parda ortidan xabar
+Go'zal bo'lmasa edi
+O'larmidi payg'ambar""")
+
+    #ikkinchi usuli "\n" yordamida qatorlarga bo'lish
+
+print("\nO'lim asli go'zaldir \nParda ortidan xabar \nGo'zal bo'lmasa edi \nO'larmidi payg'ambar")
