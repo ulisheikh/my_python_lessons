1e9b2786824bc9daf580ec146784c01249e6dbb3
ulee <ulee@kali.ulee>
2025-01-29 13:56:29 -0500

    file update

--- Anvar Narzullayev/main.py
+++ Anvar Narzullayev/main.py
@@ -1,29 +1,9 @@
-from students import *
-from tkinter import *
-from tkinter import messagebox
+from kivy.app import App
+from kivy.uix.button import Button
 
-oyna = Tk()
-oyna.title("Dasturcha :)")
-oyna.geometry("700x600")
-oyna.configure(bg="blue")
+class MyApp(App):
+    def build(self):
+        return Button(text="Salom, Kivy!")
 
-l = Label(
-    text="Talabalarning ma'lumotlarini shakllantiruvchi dastur",
-    bg = "aqua",
-    font=('arial',18,'bold'),
-    fg = "black"
-    )
-l.place(x=0,y=0,width=700,height=150)
-
-
-
-kalit = Entry()
-kalit.place(x=50,y=250,width=200, height=40)
-
-
-qiymat = Entry()
-qiymat.place(x=50,y=300,width=200, height=40)
-
-
-
-oyna.mainloop()
+if __name__ == "__main__":
+    MyApp().run()
