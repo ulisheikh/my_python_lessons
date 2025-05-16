66105bb33aa2a4db076f548596ef19e35bfcd95c
ulee <ulee@kali.kali>
2025-05-17 08:56:36 +0900

    files upgraded

--- MY_PROJECTS/students.py
+++ MY_PROJECTS/students.py
@@ -1,6 +1,8 @@
 from tkinter import Tk, Label, Entry, Button , Frame
 from openpyxl import Workbook,load_workbook
 import os
+from tkinter import messagebox
+
 
 oyna = Tk()
 oyna.title('StudentsInfo')
@@ -18,7 +20,7 @@ frame2 = Frame(oyna, bg='white')
 frame2.place(x=600, y=0, width=600, height=600)
 
 frame3 = Frame(oyna, bg='lightgray')
-frame3.place(x=0, y=600, width=1200, height=200)
+frame3.place(x=0, y=500, width=1200, height=300)
 
 
 
@@ -50,29 +52,40 @@ for text in label_texts:
 l2 = Label(frame2,width=50,height=17,bg='lightgray')
 l2.pack(side="top",pady=20)
 
+l_save = Label(frame3,text="File name: ")
+l_save.place(x=400,y=100)
 
 
+
+# Entry
+save_en = Entry(frame3,width=50)
+save_en.place(x=490,y=100)
+
+# Functions
 
 def save_data():
-
-    filename = "talabalar.xlxs"
-    if os.path.exists(filename):
-        wb = load_workbook(filename)
-        ws.activate
+    if 
+    filename = save_en.get()
+    if os.path.exists(filename + ".xlsx"):
+        wb = load_workbook(filename + ".xlsx")
+        ws = wb.active
     else:
         wb = Workbook()
         ws = wb.active
         ws.title = "Students_Info"
 
-    # 1-qatorga sarlavhalar
-    ws.append(label_texts)
+        # Faqat yangi faylga sarlavhalar yoziladi
+        ws.append(label_texts)
 
-    # 2-qatorga kiritilgan ma'lumotlar
     row_data = [entry.get() for entry in entries]
     ws.append(row_data)
 
-    wb.save('talabalar.xlsx')
+    wb.save(filename + ".xlsx")
+    messagebox.showinfo("File saqlandi ")
 
+
+save_but = Button(frame3,text="Save",height=1,width=5,command=save_data)
+save_but.place(x=900,y=200)
 
 oyna.mainloop()
 
